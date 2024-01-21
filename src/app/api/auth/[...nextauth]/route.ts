7cc320e2bed452f39ae85a219c3d950767b0a671
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-01-21 17:50:20 +0700

    Stopped tracking .env File

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,22 +3,34 @@ import mongoose from "mongoose";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from 'bcryptjs';
+import GoogleProvider from "next-auth/providers/google";
+
+interface IGoogleProvider {
+  clientId: string;
+  clientSecret: string;
+}
+
+const googleProvider: IGoogleProvider = {
+  clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+}
 
 const handler = NextAuth({
   secret: process.env.SECRET_KEY,
     providers: [
+        GoogleProvider(googleProvider),
         CredentialsProvider({
           name: 'Credentials',
           id: 'credentials',
           credentials: {
-            username: { label: "Email", type: "email", placeholder: "test@gmail.com" },
+            email: { label: "Email", type: "email", placeholder: "test@gmail.com" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
-            const email: string = credentials?.email;
-            const password: string = credentials?.password;
+            const email: string = credentials?.email ?? "";
+            const password: string = credentials?.password ?? "";
 
-            mongoose.connect(process.env.MONGO_URL);
+            mongoose.connect(process.env.MONGO_URL ?? "");
             const user = await User.findOne({email});
             const passwordOk = user && bcrypt.compareSync(password, user.password)
 
